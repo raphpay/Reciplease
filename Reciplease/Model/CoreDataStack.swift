815c11949fb875764af263f285051b2eb7f48ee4
Raphael Payet <raphpay.dev@gmail.com>
2021-06-25 20:12:21 +0200

    Rebuild core data model

---
+++ Reciplease/Model/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  Reciplease
+//
+//  Created by Raphaël Payet on 25/06/2021.
+//
+
+import Foundation
+import CoreData
+
+
+open class CoreDataStack {
+    public static let modelName = "CoreDataApp"
+    
+    public static let model: NSManagedObjectModel = {
+        let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+    
+    public init() {}
+    
+    public lazy var persistantContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: CoreDataStack.model)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    public lazy var viewContext: NSManagedObjectContext = {
+        return persistantContainer.viewContext
+    }()
+    
+    public func saveContext(_ context: NSManagedObjectContext) {
+        do {
+            try context.save()
+        } catch let error as NSError {
+            fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+}
---
+++ Reciplease/Model/Ingredients.swift
@@ -0,0 +1,14 @@
+//
+//  Ingredients.swift
+//  Reciplease
+//
+//  Created by Raphaël Payet on 25/06/2021.
+//
+
+import Foundation
+import CoreData
+
+class Ingredient: NSManagedObject {}
+
+extension Ingredient {}
+
